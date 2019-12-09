43b038de6e247057277df0f349faca78534abe98
Kenneth-29 <54361276+Kenneth-29@users.noreply.github.com>
2019-12-09 23:46:48 +0200

    9th December

--- app/src/main/java/com/mad2019/cse18_039/kpmcalculator/MainActivity.java
+++ app/src/main/java/com/mad2019/cse18_039/kpmcalculator/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+/**********************************************************************/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater mF = getMenuInflater();
